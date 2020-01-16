bbaaa9cb6e5bd8d9993701db5d7df03312a34087
Thomas Müller <thomas.mueller@tmit.eu>
2020-01-16 16:51:26 +0100

    Re-license under GPLv2

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,16 +1,22 @@
 <?php
 /**
- * ownCloud
- *
  * @author Thomas Müller <thomas.mueller@tmit.eu>
- * @copyright (C) 2019 ownCloud GmbH
- * @license ownCloud Commercial License
  *
- * This code is covered by the ownCloud Commercial License.
+ * @copyright Copyright (c) 2020, ownCloud GmbH
+ * @license GPL-2.0
  *
- * You should have received a copy of the ownCloud Commercial License
- * along with this program. If not, see
- * <https://owncloud.com/licenses/owncloud-commercial/>.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
