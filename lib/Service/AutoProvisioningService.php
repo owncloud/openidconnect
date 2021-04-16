936c3214d8cb017e7f62945c57f0f0038139369c
Tom Wezepoel <tom.wezepoel@surfsara.nl>
2021-04-16 16:13:58 +0200

    AutoProvisioning based on a Provisioning Claim

--- lib/Service/AutoProvisioningService.php
+++ lib/Service/AutoProvisioningService.php
@@ -81,15 +81,15 @@ class AutoProvisioningService {
 			throw new LoginException("Configured attribute $attribute is not known.");
 		}
 
-        $openIdConfig = $this->getOpenIdConfiguration();
-        $provisioningClaim = $openIdConfig['auto-provision']['provisioning-claim'] ?? null;
-        if ($provisioningClaim) {
-                $this->logger->debug('ProvisioningClaim is defined for auto-provision');
-                $provisioningAttribute = $openIdConfig['auto-provision']['provisioning-attribute'] ?? null;
-                if (! \in_array($provisioningAttribute, $userInfo->$provisioningClaim, true)) {
-                        throw new LoginException("Required provisioning attribute is not found.");
-                }
-        }
+		$openIdConfig = $this->getOpenIdConfiguration();
+		$provisioningClaim = $openIdConfig['auto-provision']['provisioning-claim'] ?? null;
+		if ($provisioningClaim) {
+			$this->logger->debug('ProvisioningClaim is defined for auto-provision');
+			$provisioningAttribute = $openIdConfig['auto-provision']['provisioning-attribute'] ?? null;
+			if (! \in_array($provisioningAttribute, $userInfo->$provisioningClaim, true)) {
+				throw new LoginException("Required provisioning attribute is not found.");
+			}
+		}
 
 		$userId = $this->mode() === 'email' ? \uniqid('oidc-user-') : $emailOrUserId;
 		$passwd = \uniqid('', true);
