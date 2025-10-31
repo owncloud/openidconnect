e054c2f53f754f6acd1d640f8eb3790320341377
ownClouders <devops@owncloud.com>
2025-10-31 00:04:25 +0000

    [tx] updated from transifex

---
+++ l10n/nl.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+    "openidconnect",
+    {
+    "Only guests are allowed through this authentication mechanism" : "Alleen gasten worden toegelaten via dit authenticatie mechanisme"
+},
+"nplurals=2; plural=(n != 1);");
